439a011ae18f66aff8621e701161b1636b64f15d
ihsan <ihsankocak@gmail.com>
2022-12-19 12:02:27 +0300

    first commit of initial spring intialzr generated code

---
+++ src/test/java/com/bekdik/examer/ExamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekdik.examer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
