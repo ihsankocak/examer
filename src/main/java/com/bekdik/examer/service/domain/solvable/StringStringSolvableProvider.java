6e03faab53e2ed2fa9b9e8ae7ba79e1d4bed1ac7
Ihsan Kocak <ihsankocak@gmail.com>
2023-07-11 22:10:32 +0300

    tests modified

--- src/main/java/com/bekdik/examer/service/domain/solvable/StringStringSolvableProvider.java
+++ src/main/java/com/bekdik/examer/service/domain/solvable/StringStringSolvableProvider.java
@@ -1,11 +1,15 @@
 package com.bekdik.examer.service.domain.solvable;
 
-import java.io.FileReader;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonArray;
@@ -15,68 +19,65 @@ import com.google.gson.JsonParser;
 
 @Service
 
-public  class StringStringSolvableProvider implements SolvableProvider {
-private   List<BaseSolvableWithChoosable<String, String>> solvableList;
+public class StringStringSolvableProvider implements SolvableProvider {
+    private List<BaseSolvableWithChoosable<String, String>> solvableList;
 
-	public StringStringSolvableProvider() {
-	
-	
-	try(FileReader reader = new FileReader("src/main/resources/data/stringStringSolvables.json")) {
-		// Parse the JSON data using GSON
-        JsonElement json = new JsonParser().parse(reader);
+    public StringStringSolvableProvider() {
 
-        // Get the array of complex objects from the JSON data
-        JsonArray array = json.getAsJsonObject().getAsJsonArray("questions");
+        try (InputStreamReader reader = new InputStreamReader(new ClassPathResource("data/stringStringSolvables.json").getInputStream())){
+            // Parse the JSON data using GSON
+            JsonElement json = new JsonParser().parse(reader);
 
-        // Create a list to hold the complex objects
-        List<BaseSolvableWithChoosable<String, String>> complexObjects = new ArrayList<>();
+            // Get the array of complex objects from the JSON data
+            JsonArray array = json.getAsJsonObject().getAsJsonArray("questions");
+
+            // Create a list to hold the complex objects
+            List<BaseSolvableWithChoosable<String, String>> complexObjects = new ArrayList<>();
 
 
-        // Iterate over the array of complex objects
-        for (JsonElement element : array) {
-            // Deserialize each element of the array into a complex object
-        	 JsonObject object = element.getAsJsonObject();
-        	 List<String> choiceList=new ArrayList<>();
-        	 String solution=object.get("solution").getAsString();
-        	 JsonObject choosable=object.getAsJsonObject("choosable");
-        	 for(JsonElement elementChoosable:choosable.get("choices").getAsJsonArray()) {
-        		 choiceList.add(elementChoosable.getAsString());
-        	 }
-        	 StringChoosable stringChoosable=  	new StringChoosable(choiceList, solution);
-        	StringStringSolvableChoosable complexObject =new StringStringSolvableChoosable(object.get("id").getAsLong(), object.get("problem").getAsString(), solution, stringChoosable);
-            // Add the complex object to the list
-            complexObjects.add(complexObject);
+            // Iterate over the array of complex objects
+            for (JsonElement element : array) {
+                // Deserialize each element of the array into a complex object
+                JsonObject object = element.getAsJsonObject();
+                List<String> choiceList = new ArrayList<>();
+                String solution = object.get("solution").getAsString();
+                JsonObject choosable = object.getAsJsonObject("choosable");
+                for (JsonElement elementChoosable : choosable.get("choices").getAsJsonArray()) {
+                    choiceList.add(elementChoosable.getAsString());
+                }
+                StringChoosable stringChoosable = new StringChoosable(choiceList, solution);
+                StringStringSolvableChoosable complexObject = new StringStringSolvableChoosable(object.get("id").getAsLong(), object.get("problem").getAsString(), solution, stringChoosable);
+                // Add the complex object to the list
+                complexObjects.add(complexObject);
+            }
+            solvableList = complexObjects;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        solvableList= complexObjects;
-		
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
+    }
 
-	@Override
-	public Collection<BaseSolvableWithChoosable<String, String>> getSolvables() {
-		try {
-			
-				if(solvableList==null) {
-					String problem="Aşağıdakilerden hangisinde E harfi yoktur?";
-					String solution="KA";
-					Collection<String> choices=List.of("DE","FE","KA","CE");
-					AbstractChoosable<String> stringChoosable=new StringChoosable(choices, solution);
-					solvableList= new ArrayList<BaseSolvableWithChoosable<String, String>>();
-					solvableList.add(new StringStringSolvableChoosable(1L,problem,solution,stringChoosable));
-				}
-			
-			
-		return solvableList;
-			
-		}
-		catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		return List.of();
-		}
-		
-	}
+    @Override
+    public Collection<BaseSolvableWithChoosable<String, String>> getSolvables() {
+        try {
+
+            if (solvableList == null) {
+                String problem = "Aşağıdakilerden hangisinde E harfi yoktur?";
+                String solution = "KA";
+                Collection<String> choices = List.of("DE", "FE", "KA", "CE");
+                AbstractChoosable<String> stringChoosable = new StringChoosable(choices, solution);
+                solvableList = new ArrayList<BaseSolvableWithChoosable<String, String>>();
+                solvableList.add(new StringStringSolvableChoosable(1L, problem, solution, stringChoosable));
+            }
+
+
+            return solvableList;
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            return List.of();
+        }
+
+    }
 }
